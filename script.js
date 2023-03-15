a7fc69578bd45b0e57c2ff92f99904822a7e0ce9
Joel <jp421513@gmail.com>
2023-03-15 00:50:48 -0700

    3rd commit, added more var for the prompts

--- script.js
+++ script.js
@@ -4,10 +4,19 @@ var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var lowerCasedCharacters = ['a', 'b', 'c','d', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var upperCaseCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
-var lengthOfPassword = parseInt(window.prompt("Password Length"));
+function generatePassword() {
+var passwordLength = parseInt(window.prompt("How many characters would you like your password to have?"));
+if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+  alert("Password length must be a number between 8 and 128.");
+  return "";
+}
 
-console.log(lengthOfPassword);
-console.log(typeof lengthOfPassword);
+var includeSpecialCharacters = confirm("Click OK to confirm including special special Characters.");
+var includeNumericCharacters = confirm("Click OK to confirm including special numeric Characters.");
+var includeLowerCaseCharacters = confirm("Click OK to confirm including special lowercase Characters.");
+var includeUpperCaseCharacters = confirm("Click OK to confirm including special uppercase Characters.");
+
+}
 
 var result = [];
 

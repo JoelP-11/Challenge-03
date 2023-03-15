c8fce4a768a256c9a7a2b749a4625cac69202ed4
Joel <jp421513@gmail.com>
2023-03-15 01:39:24 -0700

    5th commit finished code, and most of readme now I just need to add deployed website link.

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 // Assignment code here
 var specialCharacters = ['@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
 var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-var lowerCasedCharacters = ['a', 'b', 'c','d', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var lowerCaseCharacters = ['a', 'b', 'c','d', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var upperCaseCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
 function generatePassword() {
-var passwordLength = parseInt(window.prompt("How many characters would you like your password to have?"));
+var passwordLength = parseInt(prompt("How many characters would you like your password to have?"));
 if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
   alert("Password length must be a number between 8 and 128.");
   return "";
 }
 
-var includeSpecialCharacters = confirm("Click OK to confirm including special special Characters.");
-var includeNumericCharacters = confirm("Click OK to confirm including special numeric Characters.");
-var includeLowerCaseCharacters = confirm("Click OK to confirm including special lowercase Characters.");
-var includeUpperCaseCharacters = confirm("Click OK to confirm including special uppercase Characters.");
+var includeSpecialCharacters = confirm("Click OK to confirm including special Characters.");
+var includeNumericCharacters = confirm("Click OK to confirm including numeric Characters.");
+var includeLowerCaseCharacters = confirm("Click OK to confirm including lowercase Characters.");
+var includeUpperCaseCharacters = confirm("Click OK to confirm including uppercase Characters.");
 
 if (!includeSpecialCharacters && !includeNumericCharacters && !includeLowerCaseCharacters && !includeUpperCaseCharacters) {
   alert("At least one character type needs to be selected.");
@@ -29,7 +29,7 @@ if (includeNumericCharacters) {
   allCharacters = allCharacters.concat(numericCharacters);
 }
 if (includeLowerCaseCharacters) {
-  allCharacters = allCharacters.concat(lowerCasedCharacters);
+  allCharacters = allCharacters.concat(lowerCaseCharacters);
 }
 if (includeUpperCaseCharacters) {
   allCharacters = allCharacters.concat(upperCaseCharacters);

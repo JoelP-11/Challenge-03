b466c8e8b0dbda03b9522e6c598597b1944c12f1
Joel <jp421513@gmail.com>
2023-03-15 00:13:19 -0700

    added the var arrays

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 // Assignment code here
-var LettersArr = ["a", "b", "c", "d", "e", "f",];
+var specialCharacters = ['@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var lowerCasedCharacters = ['a', 'b', 'c','d', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var upperCaseCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
 var lengthOfPassword = parseInt(window.prompt("Password Length"));
 
 console.log(lengthOfPassword);
 console.log(typeof lengthOfPassword);
 
- var result = [];
+var result = [];
 
 for (var i = 0; i < lengthOfPassword; i++) {
   var randomIndex = Math.floor(Math.random() * lettersArr.length);
